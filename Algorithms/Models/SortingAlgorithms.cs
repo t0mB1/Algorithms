39f7cce3915e6441743d3073223c72de6716efbd
Tom Brayshaw <tombrayshaw@icloud.com>
2020-06-23 21:57:07 +0100

    cleaned

--- Algorithms/Models/SortingAlgorithms.cs
+++ Algorithms/Models/SortingAlgorithms.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Algorithms.Models.SortAlgorithmOperations;
 using Microcharts;
-using SkiaSharp;
 
 namespace Algorithms.Models
 {
