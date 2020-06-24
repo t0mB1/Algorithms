9c6631f41c95b6d6a46442fc0ef6369574f2e2e8
Tom Brayshaw <tombrayshaw@icloud.com>
2020-06-24 13:10:21 +0100

    Entity for database

---
+++ Algorithms/Database/ColourSchemeEntity.cs
@@ -0,0 +1,16 @@
+﻿using SkiaSharp;
+using SQLite;
+using Xamarin.Forms;
+
+namespace Algorithms.Database
+{
+    public class ColourSchemeEntity
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string GraphColourHex { get; set; }
+        public string TextColourHex { get; set; }
+        public bool DarkThemeIsOn { get; set; }
+    }
+}
