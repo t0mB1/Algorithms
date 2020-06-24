4d7a5db64875e91441b5d677d404bc05c637c68f
Tom Brayshaw <tombrayshaw@icloud.com>
2020-06-24 13:10:02 +0100

    added methods
    - SaveColourSchemeDb
    - UpdateColourSchemeDb
    - ColourSchemeEntity

---
+++ Algorithms/Database/ColourSchemeDatabase.cs
@@ -0,0 +1,38 @@
+﻿using SQLite;
+
+namespace Algorithms.Database
+{
+    public class ColourSchemeDatabase
+    {
+        readonly SQLiteConnection _database;
+
+        public ColourSchemeDatabase(string dbPath)
+        {
+            _database = new SQLiteConnection(dbPath);
+            _database.CreateTable<ColourSchemeEntity>();
+        }
+
+        public int SaveColourSchemeDb(ColourSchemeEntity colourScheme)
+        {
+            // only 1 entity will exist
+            _database.DeleteAll<ColourSchemeEntity>();
+            return _database.Insert(colourScheme);
+        }
+
+        public int UpdateColourScheme(ColourSchemeEntity entity)
+        {
+            return _database.Update(entity);
+        }
+
+        public ColourSchemeEntity GetColourSchemeDb()
+        {
+            ColourSchemeEntity[] entities = _database.Table<ColourSchemeEntity>()
+                                                     .ToArray();
+            if(entities.Length > 0)
+            {
+                return entities[0];
+            }
+            return null;
+        }
+    }
+}
