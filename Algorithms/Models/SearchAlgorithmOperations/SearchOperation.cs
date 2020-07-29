d5a288ba125446c2af01f569e91988fff66379bf
Tom Brayshaw <tombrayshaw@icloud.com>
2020-07-29 17:21:34 +0100

    class for a search operation

---
+++ Algorithms/Models/SearchAlgorithmOperations/SearchOperation.cs
@@ -0,0 +1,11 @@
+﻿using Microcharts;
+
+namespace Algorithms.Models.SearchAlgorithmOperations
+{
+    public class SearchOperation : ISearchOperation
+    {
+        public Entry Entry { get; set; }
+        public string ChangeToColour { get; set; }
+        public bool IsSearchItem { get; set; }
+    }
+}
