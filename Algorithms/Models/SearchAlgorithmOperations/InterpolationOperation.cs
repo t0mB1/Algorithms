91f8d4dd6f293d3cd59c205f192e3b439dd73c94
Tom Brayshaw <tombrayshaw@icloud.com>
2020-07-30 14:47:07 +0100

    new class

---
+++ Algorithms/Models/SearchAlgorithmOperations/InterpolationOperation.cs
@@ -0,0 +1,13 @@
+﻿using Microcharts;
+
+namespace Algorithms.Models.SearchAlgorithmOperations
+{
+    public class InterpolationOperation : IInterpolationOperation
+    {
+        public Entry Entry { get; set; }
+        public Entry[] Markers { get; set; }
+        public bool IsSearchItem { get; set; }
+        public string ChangeToColour { get; set; }
+        public bool SearchItemIsMarker { get; set; }
+    }
+}
