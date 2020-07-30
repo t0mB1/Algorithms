8cda33a228c48fccb842bb9d61bbb8be7a97e0b4
Tom Brayshaw <tombrayshaw@icloud.com>
2020-07-30 14:44:55 +0100

    interface for interpolationSearchOperation

---
+++ Algorithms/Models/SearchAlgorithmOperations/IInterpolationOperaton.cs
@@ -0,0 +1,10 @@
+﻿using Microcharts;
+
+namespace Algorithms.Models.SearchAlgorithmOperations
+{
+    public interface IInterpolationOperation : ISearchOperation
+    {
+        Entry[] Markers { get; set; }
+        bool SearchItemIsMarker { get; set; }
+    }
+}
