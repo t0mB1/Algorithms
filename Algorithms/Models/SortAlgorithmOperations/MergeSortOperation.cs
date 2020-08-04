631874e18bfaebde2e8baacd10c8355299604e45
Tom Brayshaw <tombrayshaw@icloud.com>
2020-08-04 19:19:55 +0100

    operation class for merge sort

---
+++ Algorithms/Models/SortAlgorithmOperations/MergeSortOperation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microcharts;
+
+namespace Algorithms.Models.SortAlgorithmOperations
+{
+    public class MergeSortOperation
+    {
+        public IEnumerable<Entry> LeftEntries { get; set; }
+        public IEnumerable<Entry> RightEntries { get; set; }
+        public IEnumerable<Entry> ResultsEntries { get; set; }
+        public bool IsFinalMergeOperation { get; set; }
+    }
+}
